8f47fa03ced3f3a04695aba7b87b577961538121
sai <ksspnit113@gmail.com>
2024-12-08 10:07:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Loginpage from './component/loginpage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,8 +17,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      {/* </header> */}
+      <Loginpage />
     </div>
   );
 }
---
+++ src/component/loginpage.js
@@ -0,0 +1,10 @@
+function loginpage(){
+    return (
+        <div>
+        <button>Login</button>
+        <button>Register</button>
+        {/* <h1>Login</h1> */}
+        </div>
+    )
+}
+export default loginpage;
